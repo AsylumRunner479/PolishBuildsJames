74ba480b7f69e1294ec6fca009a09f6f9474c165
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-09-01 14:53:37 +1000

    g

---
+++ Polish Builds Template - Tower Defense/Assets/Scripts/JamesStuff/TechnoWave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class TechnoWave : MonoBehaviour
+{
+    public VolumeProfile profile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Volume volume = gameObject.GetComponent<Volume>();
+        profile = volume.profile;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColorAdjustments adjustments;
+        if (profile.TryGet<ColorAdjustments>(out adjustments))
+        {
+            adjustments.hueShift.value += Time.deltaTime * 10;
+        }
+    }
+}
